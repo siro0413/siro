d157188271b78430887d5686b3503e750ba7efe1
siro0413 <48557104+siro0413@users.noreply.github.com>
2019-04-12 17:01:42 +0900

    Add files via upload

---
+++ 190412 DX/D2Dll/Color.hlsl
@@ -0,0 +1,15 @@
+//float4 main() : SV_TARGET
+//{
+//	return float4(1.0f, 1.0f, 1.0f, 1.0f);
+//}
+
+struct VertexInput
+{
+    float4 Position : POSITION0;
+    float4 Color : COLOR0;
+};
+
+struct PixelInput
+{
+
+};
